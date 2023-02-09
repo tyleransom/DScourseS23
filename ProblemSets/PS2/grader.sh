17e5076b08369b7beec816a5c0c1241c1f9569b6
Tyler Ransom <tyleransom@gmail.com>
2023-02-09 08:43:55 -0600

    add grading shell script for PS2

---
+++ ProblemSets/PS2/grader.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+points=0
+
+for i in $(seq 1 17); do
+    j=$(printf "%03d" $i)
+    cd /home/ouecon${j}
+    
+    # 2 -- set up ssh key
+    if  [ "$(ls -A .ssh)" ]; then
+        points=$((points + 5))
+    else
+        echo "Did not set up ssh key"
+    fi
+    
+    # 3-4 -- cloned repository
+    if [ -d "DScourseS23" ]; then
+        points=$((points + 5))
+    else
+        echo "Directory named 'DScourseS23' does not exist"
+    fi
+    
+    # 5 -- .tex file in PS2 folder
+    if ls /home/ouecon${j}/DScourseS23/ProblemSets/PS2/*_*.tex 1> /dev/null 2>&1; then
+        points=$((points + 5))
+    else
+        echo "tex file does not exist"
+    fi
+        
+    # 6 -- .pdf file in PS2 folder
+    if ls /home/ouecon${j}/DScourseS23/ProblemSets/PS2/*_*.pdf 1> /dev/null 2>&1; then
+        points=$((points + 5))
+    else
+        echo "pdf file does not exist"
+    fi
+    
+    # 7
+    
+    # 8
+    
+    # 9 -- updated bash profile file with newer R version
+    if grep -q "foss" .bash_profile; then
+        points=$((points + 5))
+    else
+        echo ".bash_profile does not contain the string 'foss'"
+    fi
+
+    # 10 -- R directory (i.e. has installed R packages)
+    if [ -d "R" ]; then
+        points=$((points + 5))
+    else
+        echo "Directory named 'R' does not exist"
+    fi
+done
+
+echo "Total points: $points"
