e495ef8f93e9d097143b094fa7ddb1e1288d3dd9
Tyler Ransom <tyleransom@gmail.com>
2023-02-09 09:05:35 -0600

    update grader script

--- ProblemSets/PS2/grader.sh
+++ ProblemSets/PS2/grader.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-points=0
-
 for i in $(seq 1 17); do
+    echo "user $i"
+    points=0
     j=$(printf "%03d" $i)
     cd /home/ouecon${j}
     
     # 2 -- set up ssh key
-    if  [ "$(ls -A .ssh)" ]; then
+    if  [ "$(ls -A .ssh 2> /dev/null)" ]; then
         points=$((points + 5))
+    elif [ $? -ne 0 ]; then
+        echo "Error with ls command for .ssh directory"
     else
         echo "Did not set up ssh key"
     fi
@@ -50,6 +52,7 @@ for i in $(seq 1 17); do
     else
         echo "Directory named 'R' does not exist"
     fi
+    echo "Total points: $points"
+    echo " "
 done
 
-echo "Total points: $points"
